6d23cab37b595514612a843c01586fdd4f33c19d
Zia Khalid <zkhalid@DELC02P22G3G3QD.sea.corp.expecn.com>
2017-01-14 17:51:02 +0530

    First Commit

---
+++ app/src/main/java/family/khalid/zia/interviewcracker/MainActivity.java
@@ -0,0 +1,13 @@
+package family.khalid.zia.interviewcracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}

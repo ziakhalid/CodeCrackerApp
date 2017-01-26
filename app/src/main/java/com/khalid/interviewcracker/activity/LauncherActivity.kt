50992e0eaf52b1eb1f8f1d0ed1ea913c4c791175
Zia Khalid <zkhalid@DELC02P22G3G3QD.sea.corp.expecn.com>
2017-01-26 19:39:01 +0530

    adding welcome text and kotlin fix

--- app/src/main/java/com/khalid/interviewcracker/activity/LauncherActivity.kt
+++ app/src/main/java/com/khalid/interviewcracker/activity/LauncherActivity.kt
@@ -1,33 +1,17 @@
 package com.khalid.interviewcracker.activity
 
-import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
-import android.widget.Toast
-import butterknife.BindView
-import butterknife.ButterKnife
 import com.interviewcracker.R
-import com.khalid.interviewcracker.ICApplication
-import javax.inject.Inject
 
 class LauncherActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var context: Context
-
-    @BindView(R.id.welcome_message)
-    internal var welcomeText: TextView? = null
+    val welcomeText: TextView by lazy { findViewById(R.id.welcome_message) as TextView }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.launcher_activity)
-        ButterKnife.bind(this)
-
-       val myApplication = application as ICApplication
-        myApplication.appComponent.inject(this)
-
-//        welcomeText!!.text = context.resources.getString(R.string.app_name)
-        Toast.makeText(context, "Welcome sir !!!", Toast.LENGTH_LONG).show()
+        welcomeText.text = resources.getString(R.string.app_name)
     }
 }

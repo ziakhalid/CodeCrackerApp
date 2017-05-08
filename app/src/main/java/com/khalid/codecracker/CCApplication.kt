282bd8a053163d5693023b58b0ca2c4ce727ba3c
Zia Khalid <zkhalid@DELC02NWGN6G3QD.sea.corp.expecn.com>
2017-05-08 16:09:46 +0530

    Fabric crashlytics intigration

--- app/src/main/java/com/khalid/codecracker/CCApplication.kt
+++ app/src/main/java/com/khalid/codecracker/CCApplication.kt
@@ -2,11 +2,13 @@ package com.khalid.codecracker
 
 import android.app.Application
 import com.codecracker.BuildConfig
+import com.crashlytics.android.Crashlytics
 import com.khalid.codecracker.dagger.AppComponent
 import com.khalid.codecracker.dagger.AppModule
 import com.khalid.codecracker.dagger.DaggerAppComponent
 import com.khalid.codecracker.utility.StethoShim
 import com.khalid.codecracker.widget.FontCache
+import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 
@@ -22,9 +24,11 @@ class CCApplication : Application() {
         appComponent = DaggerAppComponent.builder().appModule(AppModule(this)).build()
         FontCache.initialize(this)
 
+        Fabric.with(this, Crashlytics())
+        StethoShim.install(this)
+
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
-            StethoShim.install(this)
         }
 
         if (appComponent.endpointProvider().endPoint == com.khalid.codecracker.server.EndPoint.MOCK_MODE){
